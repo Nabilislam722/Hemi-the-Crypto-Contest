c83d7d2aec721c32de6d2093b6e7a2582e00d2f5
Nabil Islam <119033503+Nabilislam722@users.noreply.github.com>
2024-11-12 00:56:19 +0600

    server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -17,5 +17,5 @@ app.get('/api/transactions', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:5000/Hemi-the-Crypto-Contest`);
 });
