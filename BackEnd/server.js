81d91499bd166101948eb87218056e97335dfedc
Nabil Islam <119033503+Nabilislam722@users.noreply.github.com>
2024-11-11 17:51:44 +0600

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -17,5 +17,5 @@ app.get('/api/transactions', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on https://hemi-the-crypto-contest.vercel.app`);
 });
