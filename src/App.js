149c97ff4ccb234faf748ac3adde7cdae1ec6f94
Nabil Islam <119033503+Nabilislam722@users.noreply.github.com>
2024-11-11 18:25:43 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import TransactionLog from './components/TransactionLog';
+import TransactionChart from './components/TransactionChart';
+import axios from 'axios';
+import { title } from 'process';
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/transactions');
+        setTransactions(response.data);
+      } catch (error) {
+        console.error("Error fetching transactions:", error);
+      }
+    };
+
+    fetchTransactions();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hemi Blockchain Transaction Log</h1>
-        <TransactionLog />
+        <h1 id="Header">Hemi Blockchain Transaction Log</h1>
+        {/* Pass transactions as a prop to TransactionLog */}
+        <TransactionLog transactions={transactions} />
+        <TransactionChart transactions={transactions} />
       </header>
     </div>
   );
