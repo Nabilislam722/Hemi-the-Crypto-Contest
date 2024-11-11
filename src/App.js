ddb9ad9a9089d7031751d52fec3753847b557b88
Nabil Islam <119033503+Nabilislam722@users.noreply.github.com>
2024-11-11 18:13:40 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import TransactionLog from './components/TransactionLog';
-import TransactionChart from './components/TransactionChart';
-import axios from 'axios';
-import { title } from 'process';
+import TransactionLog from './TransactionLog';
 
 function App() {
-  const [transactions, setTransactions] = useState([]);
-
-  useEffect(() => {
-    const fetchTransactions = async () => {
-      try {
-        const response = await axios.get('https://hemi-the-crypto-contest.vercel.app');
-        setTransactions(response.data);
-      } catch (error) {
-        console.error("Error fetching transactions:", error);
-      }
-    };
-
-    fetchTransactions();
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
-        <h1 id="Header">Hemi Blockchain Transaction Log</h1>
-        {/* Pass transactions as a prop to TransactionLog */}
-        <TransactionLog transactions={transactions} />
-        <TransactionChart transactions={transactions} />
+        <h1>Hemi Blockchain Transaction Log</h1>
+        <TransactionLog />
       </header>
     </div>
   );
